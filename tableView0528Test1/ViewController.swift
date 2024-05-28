ea2fd638d4d07d16ac6bd0cc3e5832f8173fd3c2
Reimos7 <tjrvud9@gmail.com>
2024-05-28 18:24:01 +0900

    Initial Commit

---
+++ tableView0528Test1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tableView0528Test1
+//
+//  Created by 홍석평 on 5/28/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
